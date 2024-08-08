35c92b389aaac7d095ee5ac5c7071e09a4a6cdd5
vivek routhu <vivek634163@gmail.com>
2024-08-08 21:21:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description=f.read()
+
+__version__="0.0.0"
+
+REPO_NAME="Text-Summarizer"
+AUTHOR_USER_NAME="vivek"
+SRC_REPO="textSummarizer"
+AUTHOR_EMAIL="vivek634163@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url="https://github.com/vicky6341/Text-Summarizer",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
